faedf7ecb05607524ec4c6ef7aa90ed2df82e9bb
Dustin Hays <hays.dustin.u@gmail.com>
2023-05-27 20:06:45 -0700

    commit

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express()
 const PORT = 5500
 const connectDB = require('./db')
+require('dotenv').config()
 
 connectDB()
 
 app.use(express.json())
+app.use('/api/Auth', require('./auth/route'))
+
+
 const server = app.listen(PORT, () => console.log(`Server Connected to port ${PORT}`))
 
 process.on('unhandledRejection', err => {
